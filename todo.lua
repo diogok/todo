6f80e0a8d7068fa42026340ff76005adbd4d661a
Diogo Souza da Silva <manifesto@manifesto.blog.br>
2011-07-13 21:52:21 -0300

    Working o/

--- todo.lua
+++ todo.lua
@@ -1,19 +1,52 @@
 #!/usr/bin/env lua
 
-function add(db,item) 
+function explode(line,mark)
+    local parts = {}
+    for p in string.gmatch(line,"[^".. mark .."]+") do
+        table.insert(parts,p)
+    end
+    return parts
 end
 
-function done(db,number)
+function add(db,item) 
+    local time = os.time();
+    local key = time .. "_" .. os.tmpname()
+    local value = string.gsub(item,";",",");
+
+    local file,r = io.open(db,"a");
+    file:write(key .. ";" .. time .. ";" .. value .. ";todo\n")
+    file:close()
+end
+
+function done(db,n)
+    local items = list(db)
+
+    local file = io.open(db,"w+")
+    for k, v in pairs(items) do
+        if k == tonumber(n) then
+            v[4] = "done"
+        end
+        file:write(table.concat(v,";") .. "\n")
+    end
+
+    file:close()
 end
 
 function list(db)
     local list = {}
+
+    local file = io.open(db,"r")
+    for l in file:lines() do
+        table.insert(list,explode(l,";"))
+    end
+    file:close()
+
     return list
 end
 
 
 if arg[0] == "todo.lua" or arg[0] == "todo" then
-    local db = "~/.todo.db"
+    local db = os.getenv("HOME") ..  "/.todo.db"
 
     if arg[1] == "-h" or arg[1] == "--help" then
         print("Usage:  todo # list items")
@@ -30,7 +63,9 @@ if arg[0] == "todo.lua" or arg[0] == "todo" then
         print("Ok, listing.")
         local items = list(db)
         for k, v in pairs(items) do
-            print(k .. " - " .. v)
+            if v[4] == "todo" then
+                print(k .. " - " .. v[3])
+            end
         end
     end
 end
--- todo_test.lua
+++ todo_test.lua
@@ -7,10 +7,15 @@ db = "test.db"
 add(db,"Foo")
 add(db,"Bar")
 list0 = list(db)
-assert(list0[1] == "Foo")
-assert(list0[2] == "Bar")
+assert(list0[1][3] == "Foo")
+assert(list0[2][3] == "Bar")
 
 done(db,1)
 list1 = list(db)
-assert(list1[1] == "Bar")
 
+assert(list1[1][4] == "done")
+assert(list1[2][4] == "todo")
+
+os.remove("test.db")
+
+print("Passed");
