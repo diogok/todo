a596ea2c00b9afaf053985e9ecc09523269a2af3
Diogo Souza da Silva <manifesto@manifesto.blog.br>
2011-08-15 12:04:33 -0300

    initial register logic

---
+++ site/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$couch_host = "http://localhost:5984";
+$couch_user = null;
+$couch_pass = null;
+
+?>
--- site/register.php
+++ site/register.php
@@ -1,2 +1,29 @@
 <?php
+include 'RestClient.class.php';
+include 'config.php';
+
+$user = $_POST["username"];
+$pass = $_POST["password"];
+$salt = "todoist";
+
+if(strlen($user) < 1 or strlen($pass) < 1 or !preg_match("/^[a-zA-Z0-9_]+$/",$user)) {
+    header("location: index.html#invalid");
+} else {
+    $u = new StdClass;
+    $u->name = $user;
+    $u->type = "user";
+    $u->roles = array("todoist");
+    $u->salt = $salt ;
+    $u->password_sha = sha1($pass.$salt);
+    $u->_id = "org.couchdb.user:".$user;
+    $c = RestClient::post($couch_host."/_users",json_encode($u),$couch_user,$couch_pass,"application/json");
+    if($c->getResponseCode() == '201') {
+        header("location: index.html#login");
+    } else {
+        header("location: index.html#invalid");
+    }
+}
+
+
+
 ?>
