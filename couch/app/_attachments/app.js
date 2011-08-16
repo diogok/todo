b075a82de066692dbc322952fb9a3fec27a79966
Diogo Souza da Silva <manifesto@manifesto.blog.br>
2011-08-16 15:07:15 -0300

    added done button functionality

--- couch/app/_attachments/app.js
+++ couch/app/_attachments/app.js
@@ -2,10 +2,13 @@ $(function(){
 
     $("form").submit(function(){ return false; });
 
+    var items = [];
+
     function ins(item) {
+        items[item._id] = item;
         var date = new Date(item.timestamp);
         var dateStr = date.getMonth()+"/"+date.getDay()+" "+date.getHours()+":"+date.getMinutes();
-        var html = "<li>";
+        var html = "<li id='"+item._id+"'>";
         html += "<span class='date'>"+dateStr+"</span> ";
         html += "<span class='content'>"+item.content+"</span> ";
         html += "<button class='done'>Done</button> ";
@@ -13,26 +16,42 @@ $(function(){
         $("#todo").append(html);
     }
 
-    $("#add").click(function(){
-        var content = $("#input").val();
-        var item = {content: content, timestamp: new Date().getTime(), status: "todo" };
+    function post(item,s) {
         $.ajax({
             url: "../../",
             data: JSON.stringify(item),
             type:"POST",
             contentType: "application/json",
-            success: function(r) {
-                item._rev = r.rev;
-                item._id  = r.id;
-                ins(item);
-            }
+            success: s
         });
+    }
+
+    $("#add").click(function(){
+        var content = $("#input").val();
+        var item = {content: content, timestamp: new Date().getTime(), status: "todo" };
+        post(item, function(r) {
+            item._rev = r.rev;
+            item._id  = r.id;
+            ins(item);
+        });
+    });
+
+    $("#todo button.done").live("click",function(bt){
+        var line = $(bt.currentTarget).parent()[0];
+        var id = line.id;
+        var item = items[id];
+        item.status = "done";
+        post(item,null);
+        delete items[id];
+        $(line).remove();
     });
 
     $.get("_view/ordered",function(resp) {
         var data = JSON.parse(resp);
         for(var i =0;i<data.rows.length;i++) {
-           ins(data.rows[i].value);
+           if(data.rows[i].value.status == "todo") {
+               ins(data.rows[i].value);
+           }
         }
     });
 });
