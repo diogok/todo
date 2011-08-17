883f617dd20dac61610d727ca251e9661a61c751
Diogo Souza da Silva <manifesto@manifesto.blog.br>
2011-08-17 09:57:31 -0300

    listening live to changes on todo list

--- couch/app/_attachments/app.js
+++ couch/app/_attachments/app.js
@@ -7,9 +7,8 @@ $(function(){
     function ins(item) {
         items[item._id] = item;
         var date = new Date(item.timestamp * 1000);
-        console.log(item.timestamp);
         var dateStr = date.getMonth()+"/"+date.getDay()+" "+date.getHours()+":"+date.getMinutes();
-        var html = "<li id='"+item._id+"'>";
+        var html = "<li rel='"+item._id+"'>";
         html += "<span class='date'>"+dateStr+"</span> ";
         html += "<span class='content'>"+item.content+"</span> ";
         html += "<button class='done'>Done</button> ";
@@ -32,15 +31,16 @@ $(function(){
         if(content.length < 1) return;
         var item = {content: content, timestamp: new Date().getTime()/1000, status: "todo" };
         post(item, function(r) {
-            item._rev = r.rev;
-            item._id  = r.id;
+            var data = JSON.parse(r);
+            item._rev = data.rev;
+            item._id  = data.id;
             ins(item);
         });
     });
 
     $("#todo button.done").live("click",function(bt){
-        var line = $(bt.currentTarget).parent()[0];
-        var id = line.id;
+        var line = $( $(bt.currentTarget).parent()[0] );
+        var id = line.attr("rel");
         var item = items[id];
         item.status = "done";
         item.done_timestamp = new Date().getTime()/1000;
@@ -50,6 +50,31 @@ $(function(){
         });
     });
 
+    function changes(since,process) {
+        $.ajax({
+            url: "../../_changes?feed=longpoll&since="+ since,
+            success: function(r) {
+                var data = JSON.parse(r);
+                if(process) {
+                    $.get("../../"+ data.results[0].id,function(r) {
+                            var data = JSON.parse(r);
+                            if(data.status == "done") {
+                                if(typeof items[data._id] != "undefined" && items[data._id] != null) {
+                                    $("li[rel='"+ data._id +"']").remove();
+                                    delete items[data._id];
+                                }
+                            } else {
+                                if(typeof items[data._id] == "undefined" || items[data._id] == null) {
+                                    ins(data);
+                                }
+                            }
+                        });
+                }
+                changes(data.last_seq,true);
+            }
+        });
+    }
+
     $.get("_view/ordered",function(resp) {
         var data = JSON.parse(resp);
         for(var i =0;i<data.rows.length;i++) {
@@ -57,5 +82,6 @@ $(function(){
                ins(data.rows[i].value);
            }
         }
+        changes(0,false);
     });
 });
